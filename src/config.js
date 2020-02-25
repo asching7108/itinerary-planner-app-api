28c474f17cbf289a2499a0bdb8ce761feaf076e1
asching7108 <asching7108@gmail.com>
2020-02-25 15:00:01 -0500

    Setup CORS

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-	NODE_ENV: process.env.NODE_ENV || 'development'
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  CLIENT_ORIGIN: 'https://itinerary-planner-app.asching7108.now.sh'
 };
