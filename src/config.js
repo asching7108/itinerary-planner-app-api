18f45b1a411101b5fba82688ba0a993fcf6a9a51
asching7108 <asching7108@gmail.com>
2020-04-29 23:48:30 -0400

    Modify default client origin url

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ module.exports = {
 	TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://esther_lin@localhost/itinerary_planner_test',
 	JWT_SECRET: process.env.JWT_SECRET || 'jwt-secret-esther-lin-2020',
 	JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
-	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://itinerary-planner-app.asching7108.now.sh'
+	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://vamos-itinerary-planner.now.sh'
 };
